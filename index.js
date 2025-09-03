b1e0aebbe26688bbebad647bc85047d6df4140bc
Aiden696 <musicjoy333@gmail.com>
2025-09-03 20:40:43 +0300

    изменен index.js

--- index.js
+++ index.js
@@ -4,46 +4,57 @@ let inputWord = document.getElementById('inputWord');
 let btn = document.getElementById('btn')
 
 
-
 let table = document.getElementById('table');
 table.classList.add('table')
-let trAll = document.querySelectorAll('tr');
-for (let tr of trAll) {
+
+let allRow = document.querySelectorAll('tr');
+for (let tr of allRow) {
     tr.classList.add('tr')
     for (let j = 0; j <= 4; j++) {
         let td = document.createElement('td');
         td.classList.add('td')
         td.textContent = '';
-        
         tr.append(td);
     }
 }
 
-function findWord (cells) {
-    btn.addEventListener('click', function() {
-        let inputWordArr = [...inputWord.value];
-        let wordArr = [...'булка'];
+let countAttempt = 0; //попытка
 
-        let count = 0;
-        for (let i = 0; i < wordArr.length; i++) {
-            if (wordArr[i] === inputWordArr[i]) {
-                cells[i].textContent = wordArr[i]
-                cells[i].classList.add('right')
-                count++;
-            } else {
-                cells[i].textContent = inputWordArr[i]
-                cells[i].classList.add('wrong')
-            }
+btn.addEventListener('click', function() {
+    let inputWordArr = [...inputWord.value];
+    let wordArr = [...'булка'];
+
+    let rows = document.querySelectorAll('tr')
+    let currentRow = rows[countAttempt];
+    let cells = currentRow.querySelectorAll('.td')
+
+    let countLetter = compareLetters(wordArr,inputWordArr,cells);
+
+    if (countLetter === inputWordArr.length) {
+        setTimeout(() => alert('Вы выиграли!'), 1000)
+    }
+
+    countAttempt++
+    if (countAttempt === 6) {
+        setTimeout(() => alert('Закончились попытки'), 1000)
+    }
+})
+
+function compareLetters(word1,word2,cells) {
+    let countLetter = 0;
+    for (let i = 0; i < word1.length; i++) {
+        if (word1[i] === word2[i]) {
+            cells[i].textContent = word1[i]
+            cells[i].classList.add('right')
+            countLetter++;
+        } else {
+            cells[i].textContent = word2[i]
+            cells[i].classList.add('wrong')
         }
-        
-        if (count === inputWordArr.length) {
-            setTimeout(() => alert('Вы выиграли!'), 1000)
-        }
-    })
+    }
+    return countLetter;
 }
 
-let firstRow = document.querySelector('.tr1');
-let firstCells = firstRow.children;
 
-findWord (firstCells)
+
 
